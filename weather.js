308bdbfc3f927767fe196c65fcf48eda0694ec88
Alex Waller <alex@artofalexwaller.com>
2021-12-21 19:38:57 -0500

    laramieWeather

--- weather.js
+++ weather.js
@@ -7,9 +7,9 @@ console.log(cityBtn);
 
 const displayHeader = document.getElementById("display-header");
 
-const descriptionHeader = document.getElementById("description-paragraph");
+const descriptionHeader = document.getElementById("description-header");
 
-const temperatureHeader = document.getElementById("temperature-paragraph");
+const temperatureHeader = document.getElementById("temperature-header");
 
 cityBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -17,6 +17,7 @@ cityBtn.addEventListener("click", function (e) {
     `http://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&APPID=db880e70c46fa251bd18b9c84cfba4cc`
   )
     .then(response => response.json())
+    // converting from JSON to JavaScript?
     .then(data => {
       console.log(data);
       let nameValue = data["name"];
@@ -26,8 +27,10 @@ cityBtn.addEventListener("click", function (e) {
       let descValue = data["weather"][0]["description"];
 
       displayHeader.innerHTML = nameValue;
-      temperatureHeader.innerHTML = data["id"];
+      temperatureHeader.innerHTML = data["main"]["temp"];
       descriptionHeader.innerHTML = descValue;
     })
     .catch(err => console.log("Wrong City Name"));
+
+  cityInput.value = "";
 });
