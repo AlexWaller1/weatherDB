09e4cde299dbc1c52eb0983252aef076343a7370
Alex Waller <alex@artofalexwaller.com>
2022-01-02 02:27:35 -0500

    cityList

--- fiveDayWeather.js
+++ fiveDayWeather.js
@@ -147,6 +147,9 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
   const dayThreeWeather = document.createElement("h3");
   dayThreeWeather.className = "day-three-weather";
   //..................................................
+  const dayThreeImage = document.createElement("img");
+  dayThreeImage.id = "day-three-image";
+  //..................................................
   const dayDivider3 = document.createElement("h3");
   dayDivider3.className = "day-divider-3";
   dayDivider3.innerHTML = "===============";
@@ -160,6 +163,9 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
   const dayFourWeather = document.createElement("h3");
   dayFourWeather.className = "day-four-weather";
   //..................................................
+  const dayFourImage = document.createElement("img");
+  dayFourImage.id = "day-four-image";
+  //..................................................
   const dayDivider4 = document.createElement("h3");
   dayDivider4.className = "day-divider-4";
   dayDivider4.innerHTML = "===============";
@@ -172,6 +178,9 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
   //..................................................
   const dayFiveWeather = document.createElement("h3");
   dayFiveWeather.className = "day-five-weather";
+  //..................................................
+  const dayFiveImage = document.createElement("img");
+  dayFiveImage.id = "day-five-image";
   //..................................................
   const tempDivider1 = document.createElement("h3");
   tempDivider1.className = "temp-divider";
@@ -205,6 +214,9 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
       currentWeather.innerHTML = data["list"][0]["weather"][0]["description"];
       weatherInfoList.append(currentWeather);
       //........................................
+      currentImage.src = `http://openweathermap.org/img/wn/${data["list"][0]["weather"][0]["icon"]}@4x.png`;
+      weatherInfoList.append(currentImage);
+      //........................................
       weatherInfoList.append(secondDivider);
       //........................................
       weatherInfoList.append(fiveDayForecastHeader);
@@ -218,7 +230,7 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
       dayOneWeather.innerHTML = data["list"][8]["weather"][0]["description"];
       weatherInfoList.append(dayOneWeather);
       //........................................
-      dayOneImage.src = `http://openweathermap.org/img/wn/${data["list"][0]["weather"][0]["icon"]}@4x.png`;
+      dayOneImage.src = `http://openweathermap.org/img/wn/${data["list"][8]["weather"][0]["icon"]}@4x.png`;
       weatherInfoList.append(dayOneImage);
       //........................................
       weatherInfoList.append(dayDivider1);
@@ -232,7 +244,7 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
       dayTwoWeather.innerHTML = data["list"][16]["weather"][0]["description"];
       weatherInfoList.append(dayTwoWeather);
       //........................................
-      dayTwoImage.src = `http://openweathermap.org/img/wn/${data["list"][8]["weather"][0]["icon"]}@4x.png`;
+      dayTwoImage.src = `http://openweathermap.org/img/wn/${data["list"][16]["weather"][0]["icon"]}@4x.png`;
       weatherInfoList.append(dayTwoImage);
       //........................................
       weatherInfoList.append(dayDivider2);
@@ -246,6 +258,9 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
       dayThreeWeather.innerHTML = data["list"][24]["weather"][0]["description"];
       weatherInfoList.append(dayThreeWeather);
       //........................................
+      dayThreeImage.src = `http://openweathermap.org/img/wn/${data["list"][24]["weather"][0]["icon"]}@4x.png`;
+      weatherInfoList.append(dayThreeImage);
+      //........................................
       weatherInfoList.append(dayDivider3);
       //........................................
       dayFourDate.innerHTML = data["list"][32]["dt_txt"];
@@ -257,6 +272,9 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
       dayFourWeather.innerHTML = data["list"][32]["weather"][0]["description"];
       weatherInfoList.append(dayFourWeather);
       //........................................
+      dayFourImage.src = `http://openweathermap.org/img/wn/${data["list"][32]["weather"][0]["icon"]}@4x.png`;
+      weatherInfoList.append(dayFourImage);
+      //........................................
       weatherInfoList.append(dayDivider4);
       //........................................
       dayFiveDate.innerHTML = data["list"][39]["dt_txt"];
@@ -267,6 +285,9 @@ fiveDayWeatherBtn.addEventListener("click", function (e) {
       //........................................
       dayFiveWeather.innerHTML = data["list"][39]["weather"][0]["description"];
       weatherInfoList.append(dayFiveWeather);
+      //........................................
+      dayFiveImage.src = `http://openweathermap.org/img/wn/${data["list"][39]["weather"][0]["icon"]}@4x.png`;
+      weatherInfoList.append(dayFiveImage);
       //........................................
       weatherInfoList.append(tempDivider1);
       //........................................
@@ -303,6 +324,251 @@ function displayCities() {
     cityBtn.addEventListener("click", function (e) {
       e.preventDefault();
       console.log("header clicked");
+      weatherInfoList.innerHTML = "";
+      //.............................................
+      const cityName = document.createElement("h2");
+      cityName.className = "city-name-header";
+      //..............................................
+      const firstDivider = document.createElement("h3");
+      firstDivider.className = "first-divider";
+      firstDivider.innerHTML = "--------------------------------------------";
+      //..............................................
+      const currentHeader = document.createElement("h3");
+      currentHeader.className = "current-header";
+      currentHeader.innerHTML = "Current Weather";
+      //..............................................
+      const currentDate = document.createElement("h3");
+      currentDate.className = "city-date-header";
+      //.............................................
+      const currentTemperature = document.createElement("h3");
+      currentDate.className = "current-temperature-header";
+      //...................................................
+      const currentWeather = document.createElement("h3");
+      currentWeather.className = "current-weather-header";
+      //...................................................
+      const currentImage = document.createElement("img");
+      currentImage.className = "current-image";
+      //...................................................
+      const secondDivider = document.createElement("h3");
+      secondDivider.className = "second-divider";
+      secondDivider.innerHTML = "------------------------------------------";
+      //...................................................
+      const fiveDayForecastHeader = document.createElement("h3");
+      fiveDayForecastHeader.className = "five-day-forecast-header";
+      fiveDayForecastHeader.innerHTML = "Five Day Forecast";
+      //...................................................
+      const dayOneDate = document.createElement("h3");
+      dayOneDate.className = "day-one-date";
+      //...................................................
+      const dayOneTemp = document.createElement("h3");
+      dayOneTemp.className = "day-one-temp";
+      //...................................................
+      const dayOneWeather = document.createElement("h3");
+      dayOneWeather.className = "day-one-weather";
+      //...................................................
+      const dayOneImage = document.createElement("img");
+      dayOneImage.className = "day-one-image";
+      //...................................................
+      const dayDivider1 = document.createElement("h3");
+      dayDivider1.className = "day-divider";
+      dayDivider1.innerHTML = "=============";
+      //...................................................
+      const dayTwoDate = document.createElement("h3");
+      dayTwoDate.className = "day-two-date";
+      //..................................................
+      const dayTwoTemp = document.createElement("h3");
+      dayTwoTemp.className = "day-two-temp";
+      //..................................................
+      const dayTwoWeather = document.createElement("h3");
+      dayTwoWeather.className = "day-two-weather";
+      //..................................................
+      const dayTwoImage = document.createElement("img");
+      dayTwoImage.id = "day-two-image";
+      //..................................................
+      const dayDivider2 = document.createElement("h3");
+      dayDivider2.className = "day-divider-2";
+      dayDivider2.innerHTML = "==============";
+      //..................................................
+      const dayThreeDate = document.createElement("h3");
+      dayThreeDate.className = "day-three-date";
+      //..................................................
+      const dayThreeTemp = document.createElement("h3");
+      dayThreeTemp.className = "day-three-temp";
+      //..................................................
+      const dayThreeWeather = document.createElement("h3");
+      dayThreeWeather.className = "day-three-weather";
+      //..................................................
+      const dayThreeImage = document.createElement("img");
+      dayThreeImage.id = "day-three-image";
+      //..................................................
+      const dayDivider3 = document.createElement("h3");
+      dayDivider3.className = "day-divider-3";
+      dayDivider3.innerHTML = "===============";
+      //..................................................
+      const dayFourDate = document.createElement("h3");
+      dayFourDate.className = "day-four-date";
+      //..................................................
+      const dayFourTemp = document.createElement("h3");
+      dayFourTemp.className = "day-four-temp";
+      //..................................................
+      const dayFourWeather = document.createElement("h3");
+      dayFourWeather.className = "day-four-weather";
+      //..................................................
+      const dayFourImage = document.createElement("img");
+      dayFourImage.id = "day-four-image";
+      //..................................................
+      const dayDivider4 = document.createElement("h3");
+      dayDivider4.className = "day-divider-4";
+      dayDivider4.innerHTML = "===============";
+      //..................................................
+      const dayFiveDate = document.createElement("h3");
+      dayFiveDate.className = "day-five-date";
+      //..................................................
+      const dayFiveTemp = document.createElement("h3");
+      dayFiveTemp.className = "day-five-temp";
+      //..................................................
+      const dayFiveWeather = document.createElement("h3");
+      dayFiveWeather.className = "day-five-weather";
+      //..................................................
+      const dayFiveImage = document.createElement("img");
+      dayFiveImage.id = "day-five-image";
+      //..................................................
+      const tempDivider1 = document.createElement("h3");
+      tempDivider1.className = "temp-divider";
+      tempDivider1.innerHTML = "========================";
+      //..................................................
+      const tempDivider2 = document.createElement("h3");
+      tempDivider2.className = "temp-divider";
+      tempDivider2.innerHTML = "========================";
+
+      fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${city1.name}&units=imperial&appid=db880e70c46fa251bd18b9c84cfba4cc`
+      )
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          console.log(data["city"]["name"]);
+          //................................
+          cityName.innerHTML = data["city"]["name"];
+          weatherInfoList.append(cityName);
+          //................................
+          weatherInfoList.append(firstDivider);
+          //................................
+          weatherInfoList.append(currentHeader);
+          //................................
+          currentDate.innerHTML = data["list"][0]["dt_txt"];
+          weatherInfoList.append(currentDate);
+          //.....................................
+          currentTemperature.innerHTML = `${data["list"][0]["main"]["temp"]} Degrees Farenheit`;
+          weatherInfoList.append(currentTemperature);
+          //........................................
+          currentWeather.innerHTML =
+            data["list"][0]["weather"][0]["description"];
+          weatherInfoList.append(currentWeather);
+          //........................................
+          currentImage.src = `http://openweathermap.org/img/wn/${data["list"][0]["weather"][0]["icon"]}@4x.png`;
+          weatherInfoList.append(currentImage);
+          //........................................
+          weatherInfoList.append(secondDivider);
+          //........................................
+          weatherInfoList.append(fiveDayForecastHeader);
+          //........................................
+          dayOneDate.innerHTML = data["list"][8]["dt_txt"];
+          weatherInfoList.append(dayOneDate);
+          //........................................
+          dayOneTemp.innerHTML = `${data["list"][8]["main"]["temp"]} Degrees Farenheit`;
+          weatherInfoList.append(dayOneTemp);
+          //........................................
+          dayOneWeather.innerHTML =
+            data["list"][8]["weather"][0]["description"];
+          weatherInfoList.append(dayOneWeather);
+          //........................................
+          dayOneImage.src = `http://openweathermap.org/img/wn/${data["list"][8]["weather"][0]["icon"]}@4x.png`;
+          weatherInfoList.append(dayOneImage);
+          //........................................
+          weatherInfoList.append(dayDivider1);
+          //........................................
+          dayTwoDate.innerHTML = data["list"][16]["dt_txt"];
+          weatherInfoList.append(dayTwoDate);
+          //........................................
+          dayTwoTemp.innerHTML = `${data["list"][16]["main"]["temp"]} Degrees Farenheit`;
+          weatherInfoList.append(dayTwoTemp);
+          //........................................
+          dayTwoWeather.innerHTML =
+            data["list"][16]["weather"][0]["description"];
+          weatherInfoList.append(dayTwoWeather);
+          //........................................
+          dayTwoImage.src = `http://openweathermap.org/img/wn/${data["list"][16]["weather"][0]["icon"]}@4x.png`;
+          weatherInfoList.append(dayTwoImage);
+          //........................................
+          weatherInfoList.append(dayDivider2);
+          //........................................
+          dayThreeDate.innerHTML = data["list"][24]["dt_txt"];
+          weatherInfoList.append(dayThreeDate);
+          //........................................
+          dayThreeTemp.innerHTML = `${data["list"][24]["main"]["temp"]} Degrees Farenheit`;
+          weatherInfoList.append(dayThreeTemp);
+          //........................................
+          dayThreeWeather.innerHTML =
+            data["list"][24]["weather"][0]["description"];
+          weatherInfoList.append(dayThreeWeather);
+          //........................................
+          dayThreeImage.src = `http://openweathermap.org/img/wn/${data["list"][24]["weather"][0]["icon"]}@4x.png`;
+          weatherInfoList.append(dayThreeImage);
+          //........................................
+          weatherInfoList.append(dayDivider3);
+          //........................................
+          dayFourDate.innerHTML = data["list"][32]["dt_txt"];
+          weatherInfoList.append(dayFourDate);
+          //........................................
+          dayFourTemp.innerHTML = `${data["list"][32]["main"]["temp"]} Degrees Farenheit`;
+          weatherInfoList.append(dayFourTemp);
+          //........................................
+          dayFourWeather.innerHTML =
+            data["list"][32]["weather"][0]["description"];
+          weatherInfoList.append(dayFourWeather);
+          //........................................
+          dayFourImage.src = `http://openweathermap.org/img/wn/${data["list"][32]["weather"][0]["icon"]}@4x.png`;
+          weatherInfoList.append(dayFourImage);
+          //........................................
+          weatherInfoList.append(dayDivider4);
+          //........................................
+          dayFiveDate.innerHTML = data["list"][39]["dt_txt"];
+          weatherInfoList.append(dayFiveDate);
+          //........................................
+          dayFiveTemp.innerHTML = `${data["list"][39]["main"]["temp"]} Degrees Farenheit`;
+          weatherInfoList.append(dayFiveTemp);
+          //........................................
+          dayFiveWeather.innerHTML =
+            data["list"][39]["weather"][0]["description"];
+          weatherInfoList.append(dayFiveWeather);
+          //........................................
+          dayFiveImage.src = `http://openweathermap.org/img/wn/${data["list"][39]["weather"][0]["icon"]}@4x.png`;
+          weatherInfoList.append(dayFiveImage);
+          //........................................
+          weatherInfoList.append(tempDivider1);
+          //........................................
+          weatherInfoList.append(tempDivider2);
+
+          // currentImage.src = data["list"][0]["weather"][0]["icon"];
+          // weatherInfoList.append(currentImage);
+
+          //........................................
+
+          //........................................
+          let city1 = searchCity.map(c1 => c1.name);
+          //................................
+          if (!city1.includes(data["city"]["name"])) {
+            let newCity = { name: data["city"]["name"], id: count };
+            searchCity.push(newCity);
+            let JSON1 = JSON.stringify(searchCity);
+            localStorage.setItem("search-city", JSON1);
+            count++;
+            let JSON2 = JSON.stringify(count);
+            localStorage.setItem("persist-count", JSON2);
+          }
+        });
+      fiveDayWeatherInput.value = "";
     });
   });
 }
